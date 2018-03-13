6657086cd5c7fd9429f59cc002b50ce5f025d9ac
Abhimanyu <abhimanyu29.18@gmail.com>
2018-03-13 23:13:56 +0530

    created a simple parser in src/tmp/

--- assign3/test/test1.cs
+++ assign3/test/test1.cs
@@ -1,6 +1,7 @@
  class Program{
 	int a = 3;
 	char b = 'E';
+    char d = "F";
     bool c = true;
     int main(){
         a = a+4;
---
+++ assign3/test/test6.cs
@@ -0,0 +1,6 @@
+ class Program{
+    int a=1;
+    int b=4,c=a;
+    char s = 'a';
+}
+
