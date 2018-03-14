8f68ffa1da3b3737e7fcdca4114718f797346ff9
shubhoj <shubhoj@iitk.ac.in>
2018-03-15 00:17:17 +0530

    adding erroneous files

---
+++ assign3/test/testE4.cs
@@ -0,0 +1,14 @@
+ class Program{
+	int a = 3;
+	char c = 'E';
+	bool[] arr1= {true, false, true},z;
+
+	void func1(int var1, bool[] var2)   {
+		int temp = 2;
+		Print "value of temp: " + temp;
+		Print "End";
+		if(temp<=4)
+			temp=2;
+	}		
+	return 1;
+}
