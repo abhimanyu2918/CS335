c8a0a2837ad62f5b7ebb2adfd1e67c866d3d11a6
Abhimanyu <abhimanyu29.18@gmail.com>
2018-03-15 22:59:34 +0530

    updated test3

--- assign3/test/test3.cs
+++ assign3/test/test3.cs
@@ -1,8 +1,24 @@
  class Program{
-
+    int b= 2;
+    
+    int max(int a, int b){
+        return (a>b?a:b);
+    }
+    
+    int fact(int num){
+        if(num==1)
+            return num;
+        else 
+            return num*fact(num - 1);
+    }
+    
+    
     int main(){
-        a = a + 4;
-        
+        int a = a + 4;
+        int c = fact(a);
+        if(max(c,10)>11)
+            Print "Success";
+        return 0;
     }		
 	
 }
--- assign3/test/test6.cs
+++ assign3/test/test6.cs
@@ -1,5 +1,20 @@
  class Program{
     int a=1;
     int b=1;
+    int main(){
+        int i=1;
+        switch(i){
+            case 1:
+                Print "You can go";
+                break;
+            case 2:
+                Print "You can not go";
+                break;
+            default:
+                Print "What?";
+                break;
+        }
+        return 0;
+    }
 }
 
